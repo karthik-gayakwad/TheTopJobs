8ed972f98237b0ddc4eaa9e5c6d7cea388900342
Karthik <karthikgayakwad95@gmail.com>
2018-10-30 17:16:09 +0530

    Initial Project files

---
+++ TopJobs/src/main/java/com/topjobs/recruitment/TopJobs/TopJobsApplication.java
@@ -0,0 +1,13 @@
+package com.topjobs.recruitment.TopJobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopJobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopJobsApplication.class, args);
+		//System.out.println("hello");
+	}
+}
